aa305ecbc4934092fa902b1faa9945ae9c45c87a
KabirGupta1000 <77105110+KabirGupta1000@users.noreply.github.com>
2021-01-29 12:38:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+var canvas = document.getElementById("My_Olympic_canvas");
+var ctx = canvas.getContext("2d");
+var colour = "blue";
+ctx.beginPath();
+ctx.strokeStyle= colour;
+ctx.lineWidth= 2;
+ctx.arc(250,210,40,0,2*Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle= "red";
+ctx.lineWidth= 2;
+ctx.arc(250,210,40,0,2*Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle= "black";
+ctx.lineWidth= 2;
+ctx.arc(350,210,40,0,2*Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle= "yellow";
+ctx.lineWidth= 2;
+ctx.arc(250,210,40,0,2*Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle= "green";
+ctx.lineWidth= 2;
+ctx.arc(250,210,40,0,2*Math.PI);
+ctx.stroke();
+
+
